d93d0dd1211d4c511e0419300ba688d6ba0a9f11
SheikhRokon <mdrokon.u545@gmail.com>
2024-09-09 11:57:41 +0600

    problem fix

--- apps/restaurant/serializers.py
+++ apps/restaurant/serializers.py
@@ -14,9 +14,51 @@ class CategorySerializer(serializers.ModelSerializer):
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['id', 'name', 'description', 'price', 'category']
+        fields = ['id', 'name', 'description', 'price']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item = MenuItemSerializer(read_only=True)
+    item_id = serializers.PrimaryKeyRelatedField(
+        queryset=MenuItem.objects.all(), source='item', write_only=True
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'item', 'item_id', 'quantity']
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['item'] = MenuItemSerializer(instance.item).data
+        return ret
 
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, source='orderitem_set')
+
     class Meta:
         model = Order
         fields = ['id', 'customer', 'restaurant', 'items', 'total_amount', 'status', 'payment_method']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('orderitem_set')
+        order = Order.objects.create(**validated_data)
+        
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        
+        return order
+
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop('orderitem_set', None)
+        
+        # Update the order fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if items_data is not None:
+            # Clear existing items and create new ones
+            instance.orderitem_set.all().delete()
+            for item_data in items_data:
+                OrderItem.objects.create(order=instance, **item_data)
+
+        return instance
